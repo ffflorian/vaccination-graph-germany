c9a681df7ce0547f3f4d2a5d89af2ead46888efa
Florian Imdahl <git@ffflorian.de>
2021-01-14 20:52:15 +0100

    feat: Switch to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import {GoogleCharts} from 'google-charts';
 import {germany as vaccinationData, lastUpdate} from './data.json';
+const Google: typeof google = google;
 
 const vaccinations = vaccinationData.historical.reverse().map(entry => [new Date(entry.date), entry.value]);
 const lastUpdateDate = new Date(lastUpdate);
 
 function drawChart() {
-  const data = new GoogleCharts.api.visualization.arrayToDataTable([['date', 'People vaccinated'], ...vaccinations]);
+  const data = Google.visualization.arrayToDataTable([['date', 'People vaccinated'], ...vaccinations]);
 
-  const options = {
+  const options: google.visualization.AreaChartOptions = {
     hAxis: {
-      backgroundColor: '#55aa00',
       format: 'dd.MM.y',
     },
+    colors: ['#55aa00'],
     width: 900,
     height: 500,
   };
 
-  const chart = new GoogleCharts.api.visualization.AreaChart(document.getElementById('chart'));
+  const chart = new Google.visualization.AreaChart(document.getElementById('chart'));
 
   chart.draw(data, options);
 }
 
-GoogleCharts.load(drawChart, {packages: ['corechart']});
+Google.charts.load('current', {packages: ['corechart']});
+Google.charts.setOnLoadCallback(drawChart);
 
 const updatedElement = document.getElementById('updated');
 updatedElement.innerHTML = `Updated on ${lastUpdateDate.toLocaleDateString()}, ${lastUpdateDate.toLocaleTimeString()}`;
