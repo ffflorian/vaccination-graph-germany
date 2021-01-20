7823fdf61a394c6384fa2ca30954a93098a06c78
Florian Imdahl <git@ffflorian.de>
2021-01-20 13:51:53 +0100

    feat: Add number for fully vaccinated people

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import {germany as vaccinationData, lastUpdate} from './data.json';
 const Google: typeof google = google;
 
-const vaccinations = vaccinationData.historical.reverse().map(entry => [new Date(entry.date), entry.value]);
+const vaccinations = vaccinationData.historical
+  .reverse()
+  .map(entry => [new Date(entry.date), entry.peopleVaccinated, entry.peopleFullyVaccinated]);
 const lastUpdateDate = new Date(lastUpdate);
 
 function drawChart() {
-  const data = Google.visualization.arrayToDataTable([['date', 'People vaccinated'], ...vaccinations]);
+  const data = Google.visualization.arrayToDataTable([
+    ['date', 'People vaccinated', 'People fully vaccinated'],
+    ...vaccinations,
+  ]);
 
   const options: google.visualization.AreaChartOptions = {
     hAxis: {
       format: 'dd.MM.y',
     },
-    colors: ['#55aa00'],
+    colors: ['#55aa00', '#0fa8a5'],
     width: 900,
     height: 500,
   };
